76706e518b46ff67d268c997566b23162f0689dd
rodri10a <rodrigojosearguello@gmail.com>
2026-04-13 20:21:37 -0300

    Integrar middlewares de sesion y cookies en app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const routes = require('./src/routes/routes')
+const authRoutes = require('./src/routes/authRoutes')
+const { attachUser } = require('./src/middlewares/auth')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/views'))
@@ -10,7 +14,16 @@ app.set('views', path.join(__dirname, 'src/views'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'src/public')))
+app.use(cookieParser())
+app.use(session({
+  secret: 'session-secret-change-in-prod',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true, secure: false, sameSite: 'strict', maxAge: 24 * 60 * 60 * 1000 }
+}))
+app.use(attachUser)
 
+app.use('/', authRoutes)
 app.use('/', routes)
 
 app.listen(3000, () => {
